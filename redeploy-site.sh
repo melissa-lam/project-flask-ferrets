72c4bf40d7ce357970b0c299c63bfafa3353ffa9
mlam21 <mlam21@students.cs.ubc.ca>
2022-07-05 10:27:31 -0700

    updated redeploy-site.sh to use docker

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-#tmux kill-session -t portfolio_site
 cd project-flask-ferrets
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
+
+
+
+
+
+#tmux kill-session -t portfolio_site
+#source python3-virtualenv/bin/activate
+#pip install -r requirements.txt
 #systemctl daemon-reload
-systemctl restart myportfolio
+#systemctl restart myportfolio
 #tmux new -d -s portfolio_site
 #tmux send-keys -t "portfolio_site" "source python3-virtualenv/bin/activate" Enter
 #tmux send-keys -t "portfolio_site" "flask run --host=0.0.0.0" Enter
